3daf323d8ee8beb24332073f0331bf03246a7c2c
yasminevangelistapansolim01 <yasmin.evangelista01@escola.pr.gov.br>
2026-09-18 08:45:24 -0300

    Atualização do script

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const botoesCurtir = document.querySelectorAll(".curtir");
 botoesCutir.forEach(function(botaoCurtir){
     let curtu = false;
-    botaoCurtir. addEventListener("click",curtir);
+    botaoCurtir.addEventListener("click",curtir);
 function curtir(){
     const contador = botaoCurtir.querySelector("span");
     if(curtiu === false){
         contador.textContent++;
-        curtiu = true;{
+        curtiu = true;}
             else{
                 contador.textContent--;
                 curtiu = false;
