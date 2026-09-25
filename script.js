bcceab65b08be9d26fe23f33f820333e0f6dea7d
yasminevangelistapansolim01 <yasmin.evangelista01@escola.pr.gov.br>
2026-09-25 08:35:21 -0300

    alterei o style  e script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const botoesCurtir = document.querySelectorAll(".curtir");
 botoesCutir.forEach(function(botaoCurtir){
-    let curtu = false;
+    let curtiu = false;
     botaoCurtir.addEventListener("click",curtir);
 function curtir(){
     const contador = botaoCurtir.querySelector("span");
